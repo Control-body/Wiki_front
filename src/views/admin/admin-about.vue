900c97ca5fccecc6853ae05a8dbf51be08308efe
liubin_SWP <liubin@qq.com>
2022-05-08 23:09:46 +0800

    6.13 优化编辑的时候不点保存就在前端生效的问题

--- src/views/admin/admin-about.vue
+++ src/views/admin/admin-about.vue
@@ -90,7 +90,8 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
-// import {Tool} from "@/util/tool";
+import {Tool} from "../../util/tool";
+// import {Tool} from "../../util/tool";
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -223,9 +224,9 @@ export default defineComponent({
     /**
      * 编辑
      */
-    const edit = (record: any) => {
+    const edit = (record : any) => {
       modalVisible.value = true;
-      ebook.value = record;
+      ebook.value = Tool.copy(record);
       // categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
     };
     /**
