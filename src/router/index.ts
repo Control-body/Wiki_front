0ac5516a8d7beb4039c10e8ca8a3f9604222049e
liubin_SWP <liubin@qq.com>
2022-05-08 10:56:51 +0800

    6.1 将页面名称全部改成小写 让about直接引入 不用懒加载

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -10,10 +10,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+
+    // // route level code-splitting
+    // // this generates a separate chunk (about.[hash].js) for this route
+    // // which is lazy-loaded when the route is visited. 先下面是懒加载的方式
+    // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
